a4d7d763431344f3356b7c008de1997ca0a962c4
volodymyr <34801552+volodymyrvoznyuk@users.noreply.github.com>
2020-11-19 00:44:47 +0100

    seh

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
+
+/*function scrollChange(id) {
+  console.log('scroll w.height:['+ window.innerHeight +'] scroll:['+ window.scrollY +']');
+  // start with the biggest value first
+  if (window.scrollY > (window.innerHeight / 2)) {                                                                   
+      document.getElementById(id).innerHTML = 'vaardigheden';                                                       
+
+  } else if (window.scrollY > (window.innerHeight / 3)) {                                                            
+      document.getElementById(id).innerHTML = 'werk';                                                    
+
+  } else {
+      document.getElementById(id).innerHTML = 'profielschets';                                                     
+  }
+}
+window.addEventListener('scroll',function(){
+      scrollChange('sticky');                                                                                        
+  });
+  */
+
 window.addEventListener('scroll', () => {
     document.body.style.setProperty('--scroll',window.pageYOffset / (document.body.offsetHeight - window.innerHeight));
   }, false);
